5333df2746d8a47278e39e2819813ea8dc0c4f57
Matt Olsen <olsen_matthew@yahoo.com>
2016-10-01 10:32:54 -1000

    deploy

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -63,7 +63,7 @@ module.exports = {
     // There will be one main bundle, and one file per asynchronous chunk.
     // We don't currently advertise code splitting but Webpack supports it.
     filename: '../public/[name].[chunkhash:8].js',
-    chunkFilename: 'static/js/[name].[chunkhash:8].chunk.js',
+    chunkFilename: '../public/[name].[chunkhash:8].chunk.js',
     // We inferred the "public path" (such as / or /my-project) from homepage.
     publicPath: publicPath
   },
